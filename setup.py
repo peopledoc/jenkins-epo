4c2b78b19a1e91d27d0a4d1929e8b620bd75af43
Étienne BERSAC <bersace03@gmail.com>
2016-02-29 18:44:19 +0100

    Use GPLv3 or later

--- setup.py
+++ setup.py
@@ -1,5 +1,20 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+#
+# This file is part of jenkins-ghp
+#
+# jenkins-ghp is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or any later version.
+#
+# jenkins-ghp is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# jenkins-ghp.  If not, see <http://www.gnu.org/licenses/>.
+
 
 import os
 from setuptools import setup
@@ -51,7 +66,7 @@ setup(
         'Programming Language :: Python',
     ],
     keywords=['jenkins', 'github'],
-    license='MIT',
+    license='GPL v3 or later',
     url='https://github.com/novafloss/jenkins-github-poller',
     **setup_kwargs
 )
