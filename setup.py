9123668607d299b1cb27642cc9aafb85e7a23f32
Étienne BERSAC <bersace03@gmail.com>
2016-03-16 22:29:48 +0100

    Distribute freestyle.xml

--- setup.py
+++ setup.py
@@ -54,6 +54,9 @@ setup(
         'retrying',
     ],
     packages=['jenkins_ghp'],
+    package_data={
+        'jenkins_ghp': ['jobs/*.xml'],
+    },
     description='Jenkins GitHub Poller',
     author=', '.join([
         'James Pic <james.pic@people-doc.com>',
