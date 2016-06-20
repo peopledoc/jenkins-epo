5afaede4bf86dd913eca28a49ed8f92754c94875
Étienne BERSAC <etienne.bersac@people-doc.com>
2016-06-20 09:32:57 +0200

    Preparing release 1.15

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ if 0 == os.getuid():
 
 setup(
     name='jenkins-ghp',
-    version='1.15.dev0',
+    version='1.15',
     entry_points={
         'console_scripts': ['jenkins-ghp=jenkins_ghp.script:entrypoint'],
         'jenkins_ghp.bot.extensions': [
