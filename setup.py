c9a71040256d96073bca8206bc85dbca2472cd70
Étienne BERSAC <etienne.bersac@people-doc.com>
2016-06-24 15:06:04 +0200

    Back to development: 1.21

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ if 0 == os.getuid():
 
 setup(
     name='jenkins-ghp',
-    version='1.20',
+    version='1.21.dev0',
     entry_points={
         'console_scripts': ['jenkins-ghp=jenkins_ghp.script:entrypoint'],
         'jenkins_ghp.bot.extensions': [
