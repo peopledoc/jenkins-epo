d4f8242900187a71879d798a4858ac22beea2414
Étienne BERSAC <etienne.bersac@people-doc.com>
2016-07-14 14:23:43 +0200

    Don't install all zest checks

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
         ],
     },
     extras_require={
-        'release': ['wheel', 'zest.releaser[recommended]'],
+        'release': ['wheel', 'zest.releaser'],
         'test': ['freezegun', 'pytest', 'pytest-cov', 'pytest-logging'],
     },
     install_requires=[
