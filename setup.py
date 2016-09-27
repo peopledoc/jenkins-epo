0632d2f8fcad9a75ed6d792f61dace8a7d31dadd
Étienne BERSAC <etienne.bersac@people-doc.com>
2016-09-27 10:32:22 +0200

    Version 1.50

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ if 'install' not in sys.argv or 0 != os.getuid():
 
 setup(
     name='jenkins-epo',
-    version='1.49',
+    version='1.50',
     entry_points={
         'console_scripts': ['jenkins-epo=jenkins_epo.script:entrypoint'],
         'jenkins_epo.bot.extensions': [
